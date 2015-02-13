9eca25e0bbb5868b700bad06fcb158ee827c704d
HJay <exist91@gmail.com>
2015-02-13 15:29:08 +0900

    020715 스터디 실습

--- src/main/java/com/maple/test/controller/JoinController.java
+++ src/main/java/com/maple/test/controller/JoinController.java
@@ -7,8 +7,13 @@
 
 package com.maple.test.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.maple.test.model.Member;
+import com.maple.test.service.MemberService;
 
 /**
  * @author Kanghoon Choi
@@ -16,8 +21,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class JoinController {
 
+	@Autowired
+	private MemberService memberService;
+
+	@RequestMapping("/join/form")
+	public String getForm() {
+
+		System.out.println("Hello world");
+
+		return "join";// join이라는 jsp를 찾아줘!
+	}
+
 	@RequestMapping("/join")
-	public String join() {
-		return "join";
+	public String join(Model model, Member member) {
+
+		boolean result = memberService.join(member);		
+		
+		if (result) {
+			return "joinSuccess";
+		} else {
+			model.addAttribute("failMsg", "회원가입이 실패했어요");
+			model.addAttribute("member", member);
+			return "join";
+		}
 	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/joinSuccess.jsp
@@ -0,0 +1,6 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<script>
+	alert("회원 가입이 성공했습니다.")
+	location.href = "/join/form";
+</script>
